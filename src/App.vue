e9bbdadfde5595483414db778292ce907d053ae2
tenugui_taro <tenu.taro@gmail.com>
2021-02-24 14:52:09 +0900

    exeファイル追加テスト

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div name="app">
+    <transition-group appear>
+      <template v-for="comment in comments">
+        <FlowComment :key="comment.id" :comment="comment" />
+      </template>
+    </transition-group>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from '@vue/composition-api';
+import FlowComment from '@/components/FlowComment.vue';
+import CommentModel from '@/model/CommentModel';
+import firebase from '@/configs/firebase';
 
-#nav {
-  padding: 30px;
+export default defineComponent({
+  components: {
+    FlowComment,
+  },
+  setup() {
+    const state = reactive({
+      comments: [] as CommentModel[],
+      inputComment: '',
+      timeNow: Date.now(),
+    });
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    const flowLane = ref(Array.from({ length: 30 }, () => 0));
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+    const getLane = () => {
+      const index = flowLane.value.findIndex(Element => Element == 0);
+      if (index >= 0) {
+        flowLane.value[index] = 1;
+        setTimeout(() => {
+          flowLane.value[index] = 0;
+        }, 2000);
+        return index;
+      } else {
+        return Math.floor(Math.random() * Math.floor(40));
+      }
+    };
+
+    firebase
+      .database()
+      .ref('comment')
+      .orderByChild('timestamp')
+      .startAt(state.timeNow)
+      .on('child_added', function(snapshot, prevChildKey) {
+        const newPost = snapshot.val();
+        const lane = getLane();
+        const newComment = new CommentModel(
+          newPost.color,
+          newPost.text,
+          `${lane * 3}vh`,
+        );
+        state.comments.push(newComment);
+        setTimeout(() => {
+          state.comments.shift();
+        }, 10000);
+      });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
