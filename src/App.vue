2ad94e6a454c6370a2a285a3f83dbe923514b060
tenugui_taro <tenu.taro@gmail.com>
2021-03-05 03:56:54 +0900

    リファクタリング済み

--- src/App.vue
+++ src/App.vue
@@ -13,50 +13,70 @@ import { defineComponent, reactive, ref, toRefs } from '@vue/composition-api';
 import FlowComment from '@/components/FlowComment.vue';
 import CommentModel from '@/model/CommentModel';
 import firebase from '@/configs/firebase';
+import 'firebase/database';
+
+const FLOW_LANE_NUMBER = 30;
+const FLOW_LANE_OCCUPY_TIME = 2000; /*ms */
+const FLOW_TIME = 10000; /*ms */
 
 export default defineComponent({
   components: {
     FlowComment,
   },
   setup() {
+    // state init
     const state = reactive({
       comments: [] as CommentModel[],
       inputComment: '',
       timeNow: Date.now(),
     });
 
-    const flowLane = ref(Array.from({ length: 30 }, () => 0));
+    // 画面を分割
+    // 割り当て状況を保持する
+    const flowLane = ref(Array.from({ length: FLOW_LANE_NUMBER }, () => 0));
 
-    const getLane = () => {
-      const index = flowLane.value.findIndex(Element => Element == 0);
-      if (index >= 0) {
-        flowLane.value[index] = 1;
+    // レーンを割り当てる
+    const allocateFlowLane = () => {
+      const unoccupiedFlowLaneIndex = flowLane.value.findIndex(
+        Element => Element == 0,
+      );
+
+      if (unoccupiedFlowLaneIndex >= 0) {
+        // レーン割り当て
+        flowLane.value[unoccupiedFlowLaneIndex] = 1;
+
+        // レーン解放
         setTimeout(() => {
-          flowLane.value[index] = 0;
-        }, 2000);
-        return index;
+          flowLane.value[unoccupiedFlowLaneIndex] = 0;
+        }, FLOW_LANE_OCCUPY_TIME);
+        return unoccupiedFlowLaneIndex;
       } else {
-        return Math.floor(Math.random() * Math.floor(40));
+        // レーンの空きがない -> ランダムに流す
+        return Math.floor(Math.random() * 20);
       }
     };
 
+    /* firebase データ取得処理 */
     firebase
       .database()
       .ref('comment')
       .orderByChild('timestamp')
       .startAt(state.timeNow)
-      .on('child_added', function(snapshot, prevChildKey) {
+      .on('child_added', function(snapshot) {
         const newPost = snapshot.val();
-        const lane = getLane();
+        const lane = allocateFlowLane() * 3;
         const newComment = new CommentModel(
+          newPost.id,
           newPost.color,
           newPost.text,
-          `${lane * 3}vh`,
+          `${lane}vh`,
         );
         state.comments.push(newComment);
+
+        // 一定時間経過後、コメント削除
         setTimeout(() => {
           state.comments.shift();
-        }, 10000);
+        }, FLOW_TIME);
       });
 
     return {
